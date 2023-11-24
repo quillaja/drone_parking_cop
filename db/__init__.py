86a6edfd7f208f57c51199a3533a73b6c31de62d
Ben <quillaja@hotmail.com>
2023-11-23 21:23:06 -0800

    add exported types to 'db' package

---
+++ db/__init__.py
@@ -0,0 +1,5 @@
+from parking_spaces import (GeoPackageSpaceDB, ParkingSpaceDB, Permits,
+                            SpaceInfo)
+from permit import Permits
+from vehicle_registry import (CSVVehicleDB, JSONVehicleDB, VehicleDB,
+                              VehicleDict, VehicleInfo)
